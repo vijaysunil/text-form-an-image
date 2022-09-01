948c1b0a54e11b0ef244c8826c8caf58acc51b84
vijaysunil <111422231+vijaysunil@users.noreply.github.com>
2022-09-01 17:32:10 +0100

    Delete vijay1.py

--- vijay1.py
+++
@@ -1,8 +0,0 @@
-import pytesseract as tess
-tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-from PIL import Image
-
-vijay = Image.open('textquotes.png')
-text = tess.image_to_string(vijay)
-
-print(text)
